e6b7a8f6022c1ab38925e2b98a57d46de76f37ac
Quanghoang059 <quanghoanglevi@gmail.com>
2025-07-13 20:36:59 +0700

    Update render api

--- modules/rendering/render_api.py
+++ modules/rendering/render_api.py
@@ -22,7 +22,7 @@ class TextRenderer:
         self.font_color = config.get("font_color", "#000000")
         self.background_color = config.get("background_color", "#FFFFFF")
         self.line_spacing = config.get("line_spacing", 1.2)
-        self.font_path = os.path.join(PROJECT_ROOT, "fonts/simfang.ttf")
+        self.font_path = "../../fonts/simfang.ttf"
 
     def render_text(self, image, text_blocks):
         """Render text lên image"""
